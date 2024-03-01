cba9d8a5cc0455c904d306ab2940c01a0f30beae
Juan Carlos Méndez <jcmendez@alum.mit.edu>
2024-03-01 10:31:08 -0500

    Fixed formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,29 @@
-import Loading from "@/components/Loading";
-import Nav from "@/components/Nav";
-import Toast from "@/components/Toast"
-import type {Metadata} from 'next'
-import './globals.css'
-import {Suspense} from 'react';
+import Loading from '@/components/Loading';
+import Nav from '@/components/Nav';
+import Toast from '@/components/Toast';
+import type { Metadata } from 'next';
+import './globals.css';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'miPariente Backend App',
-  description: 'Caregiver and provider app',
-}
+  description: 'Caregiver and provider app'
+};
 
 export default function RootLayout({
-                                     children,
-                                   }: {
-  children: React.ReactNode
+  children
+}: {
+  children: React.ReactNode;
 }) {
   return (
-      <html lang="en">
+    <html lang="en">
       <body>
-      <Suspense fallback={<Loading/>}>
-        <Nav/>
-        {children}
-        <Toast/>
-      </Suspense>
+        <Suspense fallback={<Loading />}>
+          <Nav />
+          {children}
+          <Toast />
+        </Suspense>
       </body>
-      </html>
-  )
+    </html>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,29 @@
-"use client";
+'use client';
 
-import {withAuthenticator, WithAuthenticatorProps} from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
-import styles from './page.module.css'
-import config from "@/amplifyconfiguration.json";
-import {Amplify} from "aws-amplify";
-import ParienteList from "@/components/ParienteList";
+import {
+  withAuthenticator,
+  WithAuthenticatorProps
+} from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import styles from './page.module.css';
+import config from '@/amplifyconfiguration.json';
+import { Amplify } from 'aws-amplify';
+import ParienteList from '@/components/ParienteList';
 
-Amplify.configure(config, {ssr: true});
+Amplify.configure(config, { ssr: true });
 
 function Home({ signOut, user }: WithAuthenticatorProps) {
+  const email = user?.signInDetails?.loginId;
+  const userId = user?.username;
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <h1>Hello, {user?.username || "friend"} 👋</h1>
+        <h1>Hello, {email || 'friend'} 👋</h1>
         <ParienteList />
         <button onClick={signOut}>Sign out</button>
       </div>
     </main>
-  )
+  );
 }
 
 export default withAuthenticator(Home);
