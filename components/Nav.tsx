5539aeb49304eb83018d7bcf0aaa8339cd81684f
Juan Carlos Méndez <jcmendez@alum.mit.edu>
2024-03-01 10:33:13 -0500

    Fixed more formatting

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,16 +1,23 @@
-"use client";
+'use client';
 
 import Navbar from '@/components/Navbar';
 import { getCurrentUser } from 'aws-amplify/auth';
 
 export default function Nav() {
-  return getCurrentUser().then(function (auth) {
-    console.log(`The username: ${auth.username}`);
-    console.log(`The userId: ${auth.userId}`);
-    console.log("The signInDetails: ", auth.signInDetails);
-    return <Navbar user={auth.username} email={auth.signInDetails?.loginId || "sample@email.com"}/>;
-  }).catch(function () {
-    console.error("Unable to getCurrentUser")
-    return <Navbar user={undefined} email={undefined}/>;
-  })
+  return getCurrentUser()
+    .then(function (auth) {
+      console.log(`The username: ${auth.username}`);
+      console.log(`The userId: ${auth.userId}`);
+      console.log('The signInDetails: ', auth.signInDetails);
+      return (
+        <Navbar
+          user={auth.username}
+          email={auth.signInDetails?.loginId || 'sample@email.com'}
+        />
+      );
+    })
+    .catch(function () {
+      console.error('Unable to getCurrentUser');
+      return <Navbar user={undefined} email={undefined} />;
+    });
 }
--- components/ParienteList.tsx
+++ components/ParienteList.tsx
@@ -1,49 +1,51 @@
-"use client";
+'use client';
 
-import {useState, useEffect} from "react";
-import {generateClient} from "aws-amplify/data";
-import type {Schema} from "@/amplify/data/resource";
+import { useState, useEffect } from 'react';
+import { generateClient } from 'aws-amplify/data';
+import type { Schema } from '@/amplify/data/resource';
 
 // generate our data client using the Schema from our backend
 const client = generateClient<Schema>();
 
 export default function ParienteList() {
-  const [parientes, setParientes] = useState<Schema["Pariente"][]>([]);
+  const [parientes, setParientes] = useState<Schema['Pariente'][]>([]);
 
   const fetchParientes = async () => {
-    const { data: items, errors } = await client.models.Pariente.list()
-    setParientes(items)
-  }
+    const { data: items, errors } = await client.models.Pariente.list();
+    setParientes(items);
+  };
   function createPariente() {
     client.models.Pariente.create({
-      firstName: "Paco",
-      lastName: "Jones"
-    }).then(p => {
-      console.log(p)
-      fetchParientes().then()
-    })
+      firstName: 'Paco',
+      lastName: 'Jones'
+    }).then((p) => {
+      console.log(p);
+      fetchParientes().then();
+    });
   }
 
   function checkConnection() {
-    console.log("Client is ", client);
-    console.log("Models is ", client.models);
+    console.log('Client is ', client);
+    console.log('Models is ', client.models);
   }
 
   useEffect(() => {
-    fetchParientes().then()
+    fetchParientes().then();
   }, []);
 
-  checkConnection()
+  checkConnection();
   return (
-      <div>
-        <h1>Parientes</h1>
-        <ul>
-          {parientes.map((pariente) => (
-              <li key={pariente.id}>{pariente.firstName} {pariente.lastName}</li>
-          ))}
-        </ul>
-        <button onClick={createPariente}>Create Pariente</button>
-        <button onClick={checkConnection}>Check connection</button>
-      </div>
+    <div>
+      <h1>Parientes</h1>
+      <ul>
+        {parientes.map((pariente) => (
+          <li key={pariente.id}>
+            {pariente.firstName} {pariente.lastName}
+          </li>
+        ))}
+      </ul>
+      <button onClick={createPariente}>Create Pariente</button>
+      <button onClick={checkConnection}>Check connection</button>
+    </div>
   );
 }
